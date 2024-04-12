693a77aab432353ef1139fb31200725598056cd3
Сергей Трофимов <isip_p.s.trofimov@mpt.ru>
2024-04-12 08:51:19 +0300

    calcjs

---
+++ calcul.js
@@ -0,0 +1,29 @@
+
+        function calculator() {
+            let num1 = parseFloat(prompt("enter value:"));
+            let num2 = parseFloat(prompt("enter value2:"));
+            let operator = prompt("enter operation + - * /");
+            
+
+            
+            if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    prompt("Error");
+                    return;
+                }
+            } 
+
+            prompt("Result: " + result);
+        }
+
+        calculator();
+
+ 
